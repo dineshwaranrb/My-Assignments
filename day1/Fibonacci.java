d1ea11f79a6c5a23be19e12f6e685e8d43f6caba
DineeshwaranRB <87242006+dineshwaranrb@users.noreply.github.com>
2023-04-02 00:36:52 +0530

    Update Fibonacci.java

--- day1/Fibonacci.java
+++ day1/Fibonacci.java
@@ -4,7 +4,7 @@ public class Fibonacci {
 	public static void main(String[] args) {
 		int n = 11;
         int first = 0, second = 1;
-        System.out.print("Fibonacci Series for the first " + n + " numbers: ");
+        System.out.println("Fibonacci Series for the first " + n + " numbers: ");
         for (int i = 1; i <= n; i++) {
             System.out.print(first + " ");
             int next = first + second;
